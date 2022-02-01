f2eade100061180bbec1f47cbfa8cde8fe1b94c4
vtrain <vtrain@ms0313.utah.cloudlab.us>
2022-01-31 22:20:32 -0700

    Finished spark sort and it successfully worked!

--- spark_sort.py
+++ spark_sort.py
@@ -4,7 +4,7 @@ from pyspark.sql import SparkSession
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 spark = SparkSession.builder.appName("SortApp").getOrCreate()
-inputData = spark.read.csv(input_file).cache()
-outputData = inputData.sort("cca2", "timestamp").collect()
-outputData.write.csv(output_file)
+inputData = spark.read.option("header",True).csv(input_file).cache()
+outputData = inputData.sort("cca2", "timestamp")
+outputData.write.option("header", True).csv(output_file)
 spark.stop()
